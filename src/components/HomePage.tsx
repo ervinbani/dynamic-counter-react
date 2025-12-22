8d1d90d14e13a016403a1e1242192095d12659d8
ervinbani <bani.ervin@gmail.com>
2025-12-22 17:00:19 +0100

    Add HomePage component: create initial layout for the blog application

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+// src/components/HomePage.tsx
+import React from 'react';
+
+const HomePage: React.FC = () => (
+  <div>
+    <h1>Welcome to the Simple Blog!</h1>
+    <p>This is a demo blog application using React Router and basic authentication.</p>
+  </div>
+);
+
+export default HomePage;
