795690fa979aaa7d861bb501e82eb23d92a00ed1
ervinbani <bani.ervin@gmail.com>
2025-12-22 17:00:29 +0100

    Refactor HomePage component: format paragraph for improved readability

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,13 @@
 // src/components/HomePage.tsx
-import React from 'react';
+import React from "react";
 
 const HomePage: React.FC = () => (
   <div>
     <h1>Welcome to the Simple Blog!</h1>
-    <p>This is a demo blog application using React Router and basic authentication.</p>
+    <p>
+      This is a demo blog application using React Router and basic
+      authentication.
+    </p>
   </div>
 );
 
